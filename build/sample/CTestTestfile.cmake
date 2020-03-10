9c5ea11da88be158a6960bafd41a94095bc0541f
AnisHalikov <halickoww.an@mail.ru>
2020-03-10 16:55:46 +0300

    del

--- build/sample/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/TechTest/halikov/Variant10/sample
-# Build directory: E:/TechTest/halikov/Variant10/build/sample
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
